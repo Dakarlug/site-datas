1cf5dce307d343b650c04e8e19d51500bed06a34
aliounedia <dia.aliounes@gmail.com>
2013-12-31 21:49:54 +0100

    update

--- download.py
+++ download.py
@@ -1,6 +1,10 @@
 import urllib , os
 for url in set(open('links.md').readlines()):
-    data  = urllib.urlopen(url).read()
+    try:
+        data  = urllib.urlopen(url).read()
+    except:
+        continue
+    print 'ok'
     name  = 'datas/%s' % url.split('/')[-1]
     name  = name.replace("\n", "")
     if os.path.exists(name):
